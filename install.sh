24b35eee8cfb27f761141a7e2f6091160632c474
Stygmates <styg.tan.dat@gmail.com>
2017-12-31 19:49:41 +0100

    Update install.sh
    Ajout dépendance assimp

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
+sudo apt-get install libassimp-dev
+
 sudo apt-get install libboost-all-dev
-
-git clone https://github.com/assimp/assimp.git
-
-cmake assimp/CMakeLists.txt -G 'Unix Makefiles'
-
-make --directory assimp/
