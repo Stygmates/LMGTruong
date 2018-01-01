4f7875734dd102e1dd71dfa5e67f6711cde2658e
Stygmates <styg.tan.dat@gmail.com>
2018-01-01 04:58:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install libassimp-dev
-
-sudo apt-get install libglm-dev
-
-sudo apt-get install libglew-dev
-
-sudo apt-get install libsoil-dev
-
-sudo apt-get install freeglut3-dev
-
-sudo apt-get install libboost-all-dev
+sudo apt-get install libassimp-dev libglm-dev libglew-dev libsoil-dev freeglut3-dev libboost-all-dev
